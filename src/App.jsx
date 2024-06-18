9798f06d3ce13152498006248a7aa5adbc30c282
Lagshetti9 <lagshetti9@gmail.com>
2024-06-18 22:28:03 +0530

    1st coit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import Product from './components/Product';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <Navbar/>
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,19 @@
+import "../css/Homepage.css";
+const Product = () => {
+  return (
+    <div>
+     {/*  <div className="homepage">
+        <img src="src\assets\images\home1.png" alt="Image" height={420} />{" "}
+        <h1 className="homapage-offer"> Big Billion Days</h1>
+      </div> */}
+      <div>
+        <section>
+          <div><link rel="stylesheet" href="" /> <img src="src\assets\images\4.jpg" alt="" /></div>
+       
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
